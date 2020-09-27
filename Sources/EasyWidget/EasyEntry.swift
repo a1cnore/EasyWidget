e531f4870ea04c668924d03cc60964bc2684989e
Marvin Kleinpass <marvin.kleinpass@rwth-aachen.de>
2020-09-27 19:27:30 +0200

    added Data to parse the jsonString around

--- Sources/EasyWidget/EasyEntry.swift
+++ Sources/EasyWidget/EasyEntry.swift
@@ -8,19 +8,20 @@ import SwiftUI
 /*
  
  */
-public class EasyEntry: TimeLineEntryViewable {
+public class EasyEntry<T>: TimeLineEntryViewable {
     public var date: Date
     public var view: AnyView
+    public var data: T?
     
-    public init(_ date: Date = Date(), _ content: AnyView) {
+    public init(_ date: Date = Date(), _ content: AnyView, data: T?) {
         self.date = date
         self.view = content
+        self.data = data
+    }
+    
+    public convenience init() {
+        self.init(Date(),AnyView(Text("\(Date())")), data: nil)
     }
 }
 
-extension EasyEntry {
-    public convenience init() {
-        self.init(Date(),AnyView(Text("\(Date())")))
-    }
-}
 
